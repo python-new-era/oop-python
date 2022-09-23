5f61c75e21ab498136171294ae8b08dde130efce
saasin <hdiudhusd@aol.com>
2022-09-23 11:33:47 +0700

    Player Oop

---
+++ player.py
@@ -0,0 +1,17 @@
+'''
+
+BELAJAR MEMBUAT CLASS DI PYTHON
+'''
+
+class Player:
+    name ="Wakijo"
+    def getName(self,nama):
+        self.name = nama
+        return self.name
+
+
+pemain1 = Player()
+pemain2 = Player()
+
+print(pemain1.getName("Warjono"))
+print(pemain2.getName("Jokowi"))
